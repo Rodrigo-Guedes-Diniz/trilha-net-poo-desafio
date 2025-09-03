957c93cdecd0667af13a5a01af9619e125c6ea0f
Rodrigo-Guedes-Diniz <rg510760@gmail.com>
2025-09-02 22:28:46 -0300

    Desafio concluídogit status!

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,24 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    // DONE
+    public class Nokia : Smartphone
     {
+
+        public string Numero { get; set; }
+
+        public Nokia(string numero, string modelo, string imei, int memoria)
+        : base(numero, modelo, imei, memoria)
+        {
+
+
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        // DONE
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Aplicativo {nomeApp} instalado!");
+        }
     }
 }
